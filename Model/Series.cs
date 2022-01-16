d5ecc4f46472ca1e01bca8a78beac097191d4d10
Devashish <Devashish@DESKTOP-6ON3AII>
2022-01-16 23:49:15 +0530

    Add project files.

---
+++ Model/Series.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetflixShows.Model
+{
+    public class Series
+    {
+        public Guid Guid { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsSeries { get; set; }
+        public string Name { get; set; }
+        public int ScreeningHours { get; set; }
+        public string MyProperty { get; set; }
+
+
+    }
+}
